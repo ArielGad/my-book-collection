7b16911c7ebba3e64dc7b4b7150556c14fc237ac
ArielGad <ariel.gad4@gmail.com>
2019-09-29 15:33:52 +0300

    configure localhost:3000 as CORS FE side

--- backend/books_collection_project/settings.py
+++ backend/books_collection_project/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'book_representation',  # new
-    'rest_framework',  # new
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'rest_framework',  # new
+    'corsheaders',  # new
+    'book_representation',  # new
 ]
 
 
@@ -50,6 +52,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # new
+    'django.middleware.common.CommonMiddleware',  # new
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +62,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'  # React local default port
 ]
 
 ROOT_URLCONF = 'books_collection_project.urls'
