1b41786fc48874ec142d2b58bccc89179f1741c0
ArielGad <ariel.gad4@gmail.com>
2019-11-10 23:23:26 +0200

    add request.data['wiki_link'] = get_wiki_link(request.data['title']) to post method

--- backend/book_representation/views.py
+++ backend/book_representation/views.py
@@ -1,5 +1,5 @@
+from common.utils import get_wiki_link
 from rest_framework import generics
-
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,6 +7,10 @@ from .serializers import BookSerializer
 class ListOfBooks(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def post(self, request, *args, **kwargs):
+        request.data['wiki_link'] = get_wiki_link(request.data['title'])
+        return self.create(request, *args, **kwargs)
 
 
 class DetailBook(generics.RetrieveUpdateDestroyAPIView):
