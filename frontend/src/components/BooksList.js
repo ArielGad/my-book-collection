b99df1011afd427042d9b3b1095883d106aa07c1
ArielGad <ariel.gad4@gmail.com>
2019-11-16 19:41:48 +0200

    add ability to sort columns

--- frontend/src/components/BooksList.js
+++ frontend/src/components/BooksList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -12,6 +12,7 @@ import {connect} from 'react-redux';
 import {deleteBook} from '../actions/index';
 import CircularProgress from "@material-ui/core/CircularProgress";
 import {getFilteredBooks, needToDisplayLoader} from '../selectors/selectors';
+import _ from 'lodash';
 
 const useStyles = makeStyles({
     root: {
@@ -25,38 +26,63 @@ const useStyles = makeStyles({
 });
 
 
+
 function BooksList(props) {
+
+    const SORT_OPTIONS = {
+        NONE: list => list,
+        TITLE: list => _.orderBy(list, [item => item.title.toLowerCase()], [!needToReverseSort ? 'asc': 'desc']),
+        AUTHOR: list => _.orderBy(list, [item => item.author.toLowerCase()], [!needToReverseSort ? 'asc': 'desc']),
+        DATE_PUBLISHED: list => _.orderBy(list, [item => item.date_published], [!needToReverseSort ? 'asc': 'desc']),
+    };
+
+
+
+    const onSort = (sortBy) => {
+        setSortBy(sortBy);
+        setNeedToReverseSort(!needToReverseSort);
+    };
+
+    // sort columns related
+    const [sortBy, setSortBy] = useState('NONE');
+    const [needToReverseSort, setNeedToReverseSort] = useState(false);
+
     const classes = useStyles();
     const {books, deleteBook, isFetching} = props;
+    const sortedBooks = SORT_OPTIONS[sortBy](books);
+
     return (
         <Paper className={classes.root}>
             <div className={classes.tableWrapper}>
-                {isFetching ? <CircularProgress size={60} style={{ marginLeft:660, padding:100 }}/> : <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell><strong>Title</strong></TableCell>
-                            <TableCell align="right"><strong>Author</strong></TableCell>
-                            <TableCell align="right"><strong>Language</strong></TableCell>
-                            <TableCell align="right"><strong>Date Published</strong></TableCell>
-                            <TableCell align="right"><strong>Delete Book</strong></TableCell>
-                            <TableCell align="left"><strong>Edit Book</strong></TableCell>
-                        </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                        {books.map(book =>
-                            <TableRow key={book.id}>
-                                <TableCell component="th" scope="row"><a href={book.wiki_link} target="_blank"
-                                                                         rel="noopener noreferrer">{book.title}</a></TableCell>
-                                <TableCell align="right">{book.author}</TableCell>
-                                <TableCell align="right">{book.language}</TableCell>
-                                <TableCell align="right">{book.date_published}</TableCell>
-                                <TableCell align="right">{<DeleteButton
-                                    onClick={() => deleteBook(book.id)}/>}</TableCell>
-                                <TableCell align="left">{<EditButton bookDetails={book}/>}</TableCell>
+                {isFetching ?
+                    <CircularProgress size={60} style={{ marginLeft:660, padding:100 }}/>
+                    :
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell onClick={() => onSort('TITLE')}><strong>Title</strong></TableCell>
+                                <TableCell onClick={() => onSort('AUTHOR')} align="right"><strong>Author</strong></TableCell>
+                                <TableCell align="right"><strong>Language</strong></TableCell>
+                                <TableCell onClick={() => onSort('DATE_PUBLISHED')} align="right"><strong>Date Published</strong></TableCell>
+                                <TableCell align="right"><strong>Delete Book</strong></TableCell>
+                                <TableCell align="left"><strong>Edit Book</strong></TableCell>
                             </TableRow>
-                        )}
-                    </TableBody>
+                        </TableHead>
+
+                        <TableBody>
+                            {sortedBooks.map(book =>
+                                <TableRow key={book.id}>
+                                    <TableCell component="th" scope="row"><a href={book.wiki_link} target="_blank"
+                                                                             rel="noopener noreferrer">{book.title}</a></TableCell>
+                                    <TableCell align="right">{book.author}</TableCell>
+                                    <TableCell align="right">{book.language}</TableCell>
+                                    <TableCell align="right">{book.date_published}</TableCell>
+                                    <TableCell align="right">{<DeleteButton
+                                        onClick={() => deleteBook(book.id)}/>}</TableCell>
+                                    <TableCell align="left">{<EditButton bookDetails={book}/>}</TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
                 </Table>}
             </div>
         </Paper>
